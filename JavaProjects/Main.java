2301c0b1e0b6ac56a283dfc1068dacf4f3927729
Alin <alinalin03030@gmail.com>
2025-03-06 09:25:47 +0000

    Input from user. Display info

--- JavaProjects/Main.java
+++ JavaProjects/Main.java
@@ -1,8 +1,21 @@
 package JavaProjects;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, VS Code!");
-        Ajutor.afisare();
+        String str;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter (info/help/exit) : ");
+        str = scanner.nextLine();
+        if(str.equals("help")){
+            Ajutor.afisare();
+        }else if(str.equals("info")){
+            Ajutor.info();
+        }else if(str.equals("exit")){
+            System.exit(0);
+        }
+
+        scanner.close();
     }
 }
